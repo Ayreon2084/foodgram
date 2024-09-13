83ca04d815f6c243029b6002b4c2f02ae2e550c9
Alexander <alexander.zhukov0709@gmail.com>
2024-09-13 14:10:18 +0500

    Add admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from .models import FollowUser
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'avatar'
+    )
+    list_editable = (
+        'is_staff',
+    )
+    search_fields = ('email', 'username')
+    list_filter = ('username', 'is_staff')
+    list_display_links = ('username',)
+
+
+@admin.register(FollowUser)
+class FollowUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'user'
+    )
+    list_editable = ('user',)
+    search_fields = ('author__username',)
+    list_filter = ('author', 'user')
