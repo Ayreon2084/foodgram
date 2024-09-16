766da1eba635a7e2c2e87cb8084315e4143e6727
Alexander <alexander.zhukov0709@gmail.com>
2024-09-16 14:18:41 +0500

    Add api app djoser auth

---
+++ backend/api/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from djoser import views as djoser_views
+
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(djoser_views.UserViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    # @action me/avatar
+    # @action follow
