50fff3b59eb962d7fb79e5832f872a7525fd179c
Alexander <alexander.zhukov0709@gmail.com>
2024-09-19 15:01:59 +0500

    Update serializers, views, models for users and userfollow

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import get_user_model
+from drf_base64.fields import Base64ImageField
 from rest_framework import serializers
+
+from users.models import FollowUser
 
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -14,5 +19,36 @@ class UserSerializer(serializers.ModelSerializer):
             'username',
             'first_name',
             'last_name',
-            'avatar'
+            'avatar',
+            'is_subscribed'
         )
+
+    def get_is_subscribed(self, obj):
+
+        request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
+
+        if request.user == obj:
+            return False
+
+        return FollowUser.objects.filter(user=request.user, author=obj).exists()
+
+
+class AvatarSerializer(serializers.ModelSerializer):
+
+    avatar = Base64ImageField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('avatar',)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        request = self.context.get('request')
+
+        if instance.avatar and request:
+            avatar_url = request.build_absolute_uri(instance.avatar.url)
+            representation['avatar'] = avatar_url
+            return representation
+        return representation
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth import get_user_model
 from djoser import views as djoser_views
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import AvatarSerializer, UserSerializer
 
 User = get_user_model()
 
@@ -9,6 +14,45 @@ User = get_user_model()
 class UserViewSet(djoser_views.UserViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = LimitOffsetPagination
 
-    # @action me/avatar
+    def get_permissions(self):
+        if self.action == 'list':
+            return (AllowAny(),)
+        elif self.action == 'me':
+            return (IsAuthenticated(),)
+        return super().get_permissions()
+
+    @action(
+        detail=False,
+        url_path='me/avatar',
+        permission_classes=[IsAuthenticated],
+        methods=['put']
+    )
+    def avatar(self, request):
+        user = request.user
+
+        serializer = AvatarSerializer(
+            user,
+            data=request.data,
+            context={'request': request}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @avatar.mapping.delete
+    def delete_avatar(self, request):
+        user = request.user
+        user.avatar = None
+        user.save()
+        return Response(
+            {'detail': 'Avatar has been deleted.'},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
     # @action follow
