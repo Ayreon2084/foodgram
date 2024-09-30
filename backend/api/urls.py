ae982a473ac9338caa901bb978f576aca7a5f4de
Alexander <alexander.zhukov0709@gmail.com>
2024-09-30 13:55:47 +0300

    Add short-link-redirect to urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,24 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, TagViewSet, UserViewSet
+from .views import (
+    IngredientViewSet, RecipeViewSet,
+    TagViewSet, UserViewSet
+)
 
 api_v1 = DefaultRouter()
 api_v1.register(r'users', UserViewSet, basename='users')
 api_v1.register(r'tags', TagViewSet, basename='tags')
 api_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
+api_v1.register(r'recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
     path('', include(api_v1.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path(
+        's/<str:link_suffix>/',
+        RecipeViewSet.as_view({'get': 'redirect_short_link'}),
+        name='short-link-redirect'
+    ),
 ]
