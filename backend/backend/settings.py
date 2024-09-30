755449381121917246702fe2807791db962a8b5c
Alexander <alexander.zhukov0709@gmail.com>
2024-09-30 13:58:26 +0300

    Add default filter backend to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,6 +118,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',     
+    ],
+    'SEARCH_PARAM': 'name',
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
